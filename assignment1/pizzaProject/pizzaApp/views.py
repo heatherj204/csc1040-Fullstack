579cf4f8a1bb189e301caee1b3e9fe7b77fd9e28
Heather <heather.s.e.johnston@gmail.com>
2025-02-09 22:05:45 +0000

    Log In page added

--- assignment1/pizzaProject/pizzaApp/views.py
+++ assignment1/pizzaProject/pizzaApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from . forms import CreateUserForm
+# from django.http import HttpResponse
+from . forms import CreateUserForm, LoginForm
 
+# Authentication models and functions
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -20,7 +23,28 @@ def register(request):
     return render(request, 'register.html', context=context)
 
 def my_login(request):
-    return render(request, 'my_login.html')
+
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+
+    context = {'loginform':form}
+    return render(request, 'my_login.html', context=context)
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect('')
 
 def dashboard(request):
     return render(request, 'dashboard.html')
