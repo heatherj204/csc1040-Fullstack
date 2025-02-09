79e9316aa19c2dc81a80cad37a365c5ebb282d0e
Heather <heather.s.e.johnston@gmail.com>
2025-02-09 00:12:06 +0000

    Assignment 1 changes

--- assignment1/pizzaProject/pizzaApp/urls.py
+++ assignment1/pizzaProject/pizzaApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="homepage")
+    path('', views.index, name="homepage"),
+    path('register', views.register, name="register"),
+    path('my-login', views.my_login, name="my-login"),
+    path('dashboard', views.dashboard, name="dashboard"),
 ]
--- assignment1/pizzaProject/pizzaApp/views.py
+++ assignment1/pizzaProject/pizzaApp/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . forms import CreateUserForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect("my-login")
+
+    context = {'registerform':form}
+    return render(request, 'register.html', context=context)
+
+def my_login(request):
+    return render(request, 'my_login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
